285d4bc81d8806050f13cfe25f1fe80cf6a0b81a
mason.hows11 <mason.hows11@gmail.com>
2025-01-28 11:22:24 +0330

    fixed load image with vite in build js files

--- resources/views/livewire/register-page.blade.php
+++ resources/views/livewire/register-page.blade.php
@@ -1,13 +1,13 @@
-<div class="w-full h-svh grid lg:grid-cols-2 relative p-4 dark:bg-neutral-950">
+<div class="relative grid w-full p-4 h-svh lg:grid-cols-2 dark:bg-neutral-950">
     <section class="flex flex-col items-center justify-center gap-8">
 
-        <header class="flex flex-col items-center  gap-2 relative">
+        <header class="relative flex flex-col items-center gap-2">
 
-            <p class="absolute text-white dark:text-secondary-900/80 font-black text-6xl -mt-16 ">
+            <p class="absolute -mt-16 text-6xl font-black text-white dark:text-secondary-900/80 ">
               سیستم ساده چت
             </p>
 
-            <h1 class="font-bold text-white dark:text-secondary-300 text-lg mt-4 mb-4">
+            <h1 class="mt-4 mb-4 text-lg font-bold text-white dark:text-secondary-300">
                 چت - لذت گفت‌وگوی ناشناس
             </h1>
             <p class="text-sm text-white dark:text-secondary-400">
@@ -17,22 +17,22 @@
 
 
         <main class="w-full max-w-sm">
-            <form wire:submit.prevent="submit" class="flex flex-col gap-4 w-full">
-                <div class="flex flex-col gap-2 w-full">
-                    <div class="w-full flex items-center justify-between">
+            <form wire:submit.prevent="submit" class="flex flex-col w-full gap-4">
+                <div class="flex flex-col w-full gap-2">
+                    <div class="flex items-center justify-between w-full">
                         <label for="input-okkio-name" class="w-full text-white dark:text-secondary-300">
                             نام نمایشی
                             <span class="text-rose-500">*</span>
                         </label>
 
-                        <div x-data="{ show: false }" @mouseEnter="show = true" @mouseLeave="show = false" class="cursor-pointer relative flex items-center justify-center">
+                        <div x-data="{ show: false }" @mouseEnter="show = true" @mouseLeave="show = false" class="relative flex items-center justify-center cursor-pointer">
                             <svg
-                                class="w-4 h-4 hover:text-primary-600 transition-all duration-500"
+                                class="w-4 h-4 transition-all duration-500 hover:text-primary-600"
                                 :class="show ? 'text-primary-600' : 'text-secondary-400'"
                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
                             </svg>
-                            <div x-cloak x-show="show" x-transition class="absolute -top-20 text-sm w-64 px-4 text-justify text-white max-w-sm bg-red-500 rounded-md py-2 z-20">
+                            <div x-cloak x-show="show" x-transition class="absolute z-20 w-64 max-w-sm px-4 py-2 text-sm text-justify text-white bg-red-500 rounded-md -top-20">
                                 از اشتراک گذاری اطلاعات تخمی و خطرناک مانند شماره موبایل در این قسمت کاملا خودداری نمایید.
                             </div>
                         </div>
@@ -42,9 +42,7 @@
                         id="input-okkio-name"
                         type="text"
                         placeholder="نام کاربری خود را وارد کنید"
-                        class="w-full rounded px-4 py-2 outline-none ring-1 ring-secondary-100 focus:ring-primary-500
-                        transition-all duration-500 dark:bg-secondary-900 dark:ring-secondary-800
-                        dark:placeholder:text-secondary-500 dark:text-secondary-300"
+                        class="w-full px-4 py-2 transition-all duration-500 rounded outline-none ring-1 ring-secondary-100 focus:ring-primary-500 dark:bg-secondary-900 dark:ring-secondary-800 dark:placeholder:text-secondary-500 dark:text-secondary-300"
                     >
                     @error('display_name')
                     <p class="text-sm text-rose-500">{{ $message }}</p>
@@ -52,8 +50,7 @@
                 </div>
 
                 <div>
-                    <button type="submit" class="bg-blue-600 w-full px-4 py-2 rounded-md text-white
-                    hover:bg-primary-500 transition-all duration-500 dark:bg-primary-700 dark:hover:bg-primary-600">
+                    <button type="submit" class="w-full px-4 py-2 text-white transition-all duration-500 bg-blue-600 rounded-md hover:bg-primary-500 dark:bg-primary-700 dark:hover:bg-primary-600">
                         ورود به صفحه چت
                     </button>
                 </div>
@@ -72,13 +69,10 @@
 
     </section>
 
-    <aside class="hidden lg:flex overflow-hidden relative items-center justify-center">
+    <aside class="relative items-center justify-center hidden overflow-hidden lg:flex">
         <div class="w-full h-full overflow-hidden rounded-lg">
-            <img
-                class="w-full h-full object-center object-cover"
-                src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/covers/live-chat.png') }}"
-                alt=""
-            >
+            <img class="object-cover object-center w-full h-full"
+                src="{{ \Illuminate\Support\Facades\Vite::asset('resources/images/covers/live-chat.png') }}" alt="" >
         </div>
     </aside>
 </div>
